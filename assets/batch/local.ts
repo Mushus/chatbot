cbc192b503bdff814ef4ae58ddbf634db2d727c1
Mushus <6138982+Mushus@users.noreply.github.com>
2024-07-12 05:45:50 +0900

    fix type

--- assets/batch/local.ts
+++ assets/batch/local.ts
@@ -1,4 +1,20 @@
 import 'dotenv/config';
 import { handler } from './index';
+import { Context } from 'aws-lambda';
 
-handler(null, null as any, () => {});
+const context: Context = {
+  awsRequestId: '123',
+  callbackWaitsForEmptyEventLoop: true,
+  functionName: 'test',
+  functionVersion: '1',
+  invokedFunctionArn: 'arn',
+  logGroupName: 'test',
+  logStreamName: 'test',
+  memoryLimitInMB: '128',
+  done: () => {},
+  fail: () => {},
+  getRemainingTimeInMillis: () => 1000,
+  succeed: () => {},
+};
+
+handler(null, context, () => {});
