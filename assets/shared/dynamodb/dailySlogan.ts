39d25119e9ba8981be4a0b08d648eaf6ed4aed74
Mushus <6138982+Mushus@users.noreply.github.com>
2024-07-13 16:02:15 +0900

    out lodash in es-toolkit

--- assets/shared/dynamodb/dailySlogan.ts
+++ assets/shared/dynamodb/dailySlogan.ts
@@ -1,5 +1,5 @@
 import { BatchWriteCommand, GetCommand } from '@aws-sdk/lib-dynamodb';
-import chunk from 'lodash/chunk';
+import { chunk } from 'es-toolkit/array';
 import * as v from 'valibot';
 import { MastodonDomain, TableName } from '../env/value';
 import { pad0 } from '../util';
